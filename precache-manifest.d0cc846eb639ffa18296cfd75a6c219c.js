ce7a02b51d58dc48b4c1e9908997ea158a37b57f
rateexchange <85326988+rateexchange@users.noreply.github.com>
2021-06-04 08:47:01 +0430

    Add files via upload

---
+++ precache-manifest.d0cc846eb639ffa18296cfd75a6c219c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396434eddca3d037656c",
+    "url": "/css/app.413ae05f.css"
+  },
+  {
+    "revision": "5474863a7a5740f80a0bf281ebe55cd6",
+    "url": "/icons8-currency-exchange-48.png"
+  },
+  {
+    "revision": "9d593a1acec764fc2f52a972369755fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "396434eddca3d037656c",
+    "url": "/js/app.f60664c6.js"
+  },
+  {
+    "revision": "7f93b23f0b04a110aa9b",
+    "url": "/js/chunk-vendors.dfa5d75c.js"
+  },
+  {
+    "revision": "198d8036b2b6612b90bf570d0d102300",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
